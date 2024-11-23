d52795acacf6642f4d7d695550a6370308485d66
sameershaik-coder <sameershaik.coder@gmail.com>
2024-11-23 05:47:17 +0000

    added db settings : done

--- iodplatform/iodplatform/settings.py
+++ iodplatform/iodplatform/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'iodplatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ioddb',
+        'USER': 'ioduser',
+        'PASSWORD': 'Test@123',
+        'HOST': 'localhost',  # Set to '127.0.0.1' or the hostname of your PostgreSQL server
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
